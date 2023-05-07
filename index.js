f0e87a6076883d961d0ae8bd5844c531601136f4
lis531 <82408934+lis531@users.noreply.github.com>
2023-05-07 16:51:37 +0200

    big update
    new design, new order by, new categories, a lot of improvements

--- index.js
+++ index.js
@@ -1,97 +1,150 @@
 const date = new Date();
 
-let tweets = [];
+let news = [];
 
 const newsNum = document.querySelectorAll('.news');
+const newsContainer = document.querySelectorAll('.content');
+const lineBetween = document.querySelectorAll('.lineBetween');
 
-const rssUrl = 'https://www.wired.com/feed/category/science/latest/rss';
+let rssUrls = [
+    'https://www.wired.com/feed/category/science/robots/rss',
+    'https://www.wired.com/feed/tag/ai/latest/rss',
+    'https://www.wired.com/feed/category/science/space/rss'
+];
 
-fetch(rssUrl)
-  .then(response => response.text())
-  .then(str => {
-    return new window.DOMParser().parseFromString(str, 'text/xml');
-  })
-  .then(data => {
-    const items = data.querySelectorAll('item');
-    items.forEach(item => {
-        const title = item.querySelector('title').textContent;
-        const link = item.querySelector('link').textContent;
-        const pubDate = new Date(item.querySelector('pubDate').textContent);
-        const description = item.querySelector('description').textContent;
-        const creator = item.querySelector('creator').textContent;
-        const publisher = item.querySelector('publisher').textContent;
-        const thumbnail = item.querySelector('thumbnail').getAttribute('url');
-
-        tweets.push({
-            image: thumbnail,
-            header: title,
-            author: creator,
-            date: pubDate,
-            description: description,
-            publisher: publisher,
-            link: link
-            });              
+const fetchRssData = (rssUrl) => {
+    return fetch(rssUrl)
+    .then((response) => response.text())
+    .then((str) => {
+        return new window.DOMParser().parseFromString(str, 'text/xml');
+    })
+    .then((data) => {
+        const items = data.querySelectorAll('item');
+        items.forEach((item) => {
+            const title = item.querySelector('title').textContent;
+            const link = item.querySelector('link').textContent;
+            const pubDate = new Date(item.querySelector('pubDate').textContent);
+            const description = item.querySelector('description').textContent;
+            const creator = item.querySelector('creator').textContent;
+            const publisher = item.querySelector('publisher').textContent;
+            const thumbnail = item.querySelector('thumbnail').getAttribute('url');
+      
+            news.push({
+                image: thumbnail,
+                header: title,
+                author: creator,
+                date: pubDate,
+                description: description,
+                publisher: publisher,
+                link: link,
+            });
         });
-        orderByLatest(tweets);
+        orderByLatest(news);
     });
+};
+  
 
-const defaultOrder = () =>{
+const defaultOrder = () => {
     for (let n = 0; n < newsNum.length; n++) {
-        const images = newsNum[n].querySelectorAll('img');
-        for (let i = 0; i < images.length; i++) {
-            images[i].src = tweets[n].image;
+        if (n < news.length) {
+            const images = newsNum[n].querySelectorAll('img');
+            for (let i = 0; i < images.length; i++) {
+                images[i].src = news[n].image;
+            }
+            const headings = newsNum[n].querySelectorAll('a');
+            for (let h = 0; h < headings.length; h++) {
+                headings[h].innerHTML = news[n].header;
+                headings[h].href = news[n].link;
+            }
+            const author = newsNum[n].querySelector('.newsInfo p:first-of-type');
+            author.innerHTML = 'Author ' + news[n].author;
+            const date = newsNum[n].querySelector('.newsInfo p:last-of-type');
+            date.innerHTML = 'Date ' + news[n].date.toLocaleDateString('pl-PL');
+            newsContainer[n].style.display = 'flex';
+            lineBetween[n].style.display = 'flex';
+        } else {
+            newsContainer[n].style.display = 'none';
+            lineBetween[n].style.display = 'none';
         }
-        const headings = newsNum[n].querySelectorAll('a');
-        for (let h = 0; h < headings.length; h++) {
-            headings[h].innerHTML = tweets[n].header;
-            headings[h].href = tweets[n].link;
-        }
-        const author = newsNum[n].querySelector('.newsDesc p:first-of-type');
-        author.innerHTML = tweets[n].author;
-        const date = newsNum[n].querySelector('.newsDesc p:last-of-type');
-        date.innerHTML = tweets[n].date.toLocaleDateString();
     }
-}
+};
 
-const orderByLatest = () =>{
-    tweets.sort((a, b) => (a.date < b.date) ? 1 : -1);
+const orderByLatest = () => {
+    news.sort((a, b) => (a.date < b.date ? 1 : -1));
     defaultOrder();
-}
-  
-const orderByPopular = () =>{
-    tweets.sort((a, b) => (a.likes < b.likes) ? 1 : -1);
+};
+
+const orderByOldest = () => {
+    news.sort((a, b) => (a.date > b.date ? 1 : -1));
     defaultOrder();
-}
-  
-const orderByImportant = () =>{
-    tweets.sort((a, b) => (a.important < b.important) ? 1 : -1);
+};
+
+const orderByName = () => {
+    news.sort((a, b) => (a.header > b.header ? 1 : -1));
     defaultOrder();
-}
+};
+
+const orderLatest = document.getElementById('latest');
+const orderOldest = document.getElementById('oldest');
+const orderName = document.getElementById('lastButton');
+orderLatest.addEventListener('click', orderByLatest);
+orderOldest.addEventListener('click', orderByOldest);
+orderName.addEventListener('click', orderByName);
+
+rssUrls.forEach((rssUrl) => {
+    fetchRssData(rssUrl);
+});
+
+/* Categories */
+
+const all = document.getElementById('all');
+const space = document.getElementById('space');
+const ai = document.getElementById('ai');
+const robots = document.getElementById('robots');
+all.addEventListener('click', () => {
+    rssUrls = [
+        'https://www.wired.com/feed/category/science/robots/rss',
+        'https://www.wired.com/feed/tag/ai/latest/rss',
+        'https://www.wired.com/feed/category/science/space/rss'
+    ];
+    news = [];
+    rssUrls.forEach((rssUrl, index) => {
+        fetchRssData(rssUrl, index);
+    });
+});
   
-const orderLatest = document.getElementById("latest");
-const orderPopular = document.getElementById("popular");
-const orderImportant = document.getElementById("lastButton");
-orderLatest.addEventListener("click", orderByLatest);
-orderPopular.addEventListener("click", orderByPopular);
-orderImportant.addEventListener("click", orderByImportant);
+space.addEventListener('click', () => {
+    const rssUrl = 'https://www.wired.com/feed/category/science/space/rss';
+    news = [];
+    fetchRssData(rssUrl);
+});
+  
+ai.addEventListener('click', () => {
+    const rssUrl = 'https://www.wired.com/feed/tag/ai/latest/rss';
+    news = [];
+    fetchRssData(rssUrl);
+});
+  
+robots.addEventListener('click', () => {
+    const rssUrl = 'https://www.wired.com/feed/category/science/robots/rss';
+    news = [];
+    fetchRssData(rssUrl);
+});  
   
 
 /* footer */
-const footer = document.getElementById("bottom")
+const footer = document.getElementById('bottom');
 footer.innerHTML = `Borys Gajewski © ${date.getFullYear()}`;
 
 /* other */
-const home = document.getElementById("home");
+const theme = document.getElementById('switch');
 
-const theme = document.getElementById("switch");
-
-const changeTheme = () =>{
-    //theme.innerHTML = "toggle_off";
-    if (theme.innerHTML == "toggle_off"){
-        theme.innerHTML = "toggle_on";
-    }else{
-        theme.innerHTML = "toggle_off";
+const changeTheme = () => {
+    if (theme.innerHTML == 'toggle_off') {
+        theme.innerHTML = 'toggle_on';
+    } else {
+        theme.innerHTML = 'toggle_off';
     }
-}
+};
 
-theme.addEventListener("click", changeTheme);
+theme.addEventListener('click', changeTheme);
